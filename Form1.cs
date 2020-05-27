e9192593aa50a58ed0d3bd5398c578072c87a70b
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-05-26 21:07:14 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[] pictureBoxes = new PictureBox[63];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mouseEnterSquare(object sender, EventArgs e)
+        {
+            PictureBox square = ((PictureBox)sender);
+            square.BackColor = Color.White;
+        }
+
+        private void mouseLeaveSquare(object sender, EventArgs e)
+        {
+            PictureBox square = ((PictureBox)sender);
+            square.BackColor = default(Color);
+        }
+    }
+}
